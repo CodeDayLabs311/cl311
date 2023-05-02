43e6113d4e852942a178dbe77905416716363617
Andrey Butenko <andreyxbutenko@gmail.com>
2023-05-01 23:11:01 -0700

    Specify subdirectory

--- infra-cdk/lib/stacks/PipelineStack.ts
+++ infra-cdk/lib/stacks/PipelineStack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 // import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import * as pipelines from 'aws-cdk-lib/pipelines';
 
+const INFRA_DIRECTORY = 'infra-cdk';
+
 export class PipelineStack extends cdk.Stack {
     private pipeline: pipelines.CodePipeline;
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,7 +14,8 @@ export class PipelineStack extends cdk.Stack {
             pipelineName: 'CL311-Pipeline',
             synth: new pipelines.ShellStep('Synth', {
                 input: pipelines.CodePipelineSource.gitHub('CodeDayLabs311/cl311', 'initial-setup'),
-                commands: ['cd infra-cdk', 'npm ci', 'npm run build', 'npx cdk synth'],
+                commands: [`cd ${INFRA_DIRECTORY}`, 'npm ci', 'npm run build', 'npx cdk synth'],
+                primaryOutputDirectory: `${INFRA_DIRECTORY}/cdk.out`,
             }),
         });
     }
