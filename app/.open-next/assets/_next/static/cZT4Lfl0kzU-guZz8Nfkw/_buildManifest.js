719f3b9f737d4211b1a809cf97b9da71c8b61e10
Andrey Butenko <andreyxbutenko@gmail.com>
2023-05-02 22:40:24 -0700

    remove .open-next

--- app/.open-next/assets/_next/static/cZT4Lfl0kzU-guZz8Nfkw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/43cdd2d46cac7de0.css","static/chunks/pages/index-adcd7efd21a087cc.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
