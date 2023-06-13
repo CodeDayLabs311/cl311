fc539eb15c76358cf734a9e2b97fd3b5b924c577
Andrey Butenko <andreyxbutenko@gmail.com>
2023-06-12 22:10:11 -0700

    Create lambda execution role

--- infra-cdk/lib/stacks/NextJsStack.ts
+++ infra-cdk/lib/stacks/NextJsStack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Nextjs } from 'cdk-nextjs-standalone';
 import { Construct } from 'constructs';
 import { ENVIRONMENT } from '../core/constants';
@@ -13,9 +14,24 @@ export class NextJsStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: NextJsStackProps) {
         super(scope, id, props);
 
+        const lambdaExecutionRole = new iam.Role(this, `LambdaExecutionRole-${props.stage}`, {
+            assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+            roleName: `lambda-execution-role-${props.stage}`,
+        });
+        const dynamoDBReadPolicy = new iam.PolicyStatement({
+            actions: ['dynamodb:Scan', 'dynamodb:GetItem'],
+            resources: ['arn:aws:dynamodb:*:*:table/*'],
+        });
+        lambdaExecutionRole.addToPolicy(dynamoDBReadPolicy);
+
         new Nextjs(this, `NextJs-${props.stage}`, {
             nextjsPath: NEXTJS_PATH,
             environment: ENVIRONMENT,
+            defaults: {
+                lambda: {
+                    role: lambdaExecutionRole,
+                },
+            },
             // defaults: {
             //     distribution: {
             //         customDomain: {
