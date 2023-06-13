2e3875347158b3c5cedd9f5559f893da2fe38435
Andrey Butenko <andreyxbutenko@gmail.com>
2023-06-12 22:04:10 -0700

    Use env aws creds for debugging

--- app/src/utils/api/dynamodb.ts
+++ app/src/utils/api/dynamodb.ts
@@ -5,10 +5,10 @@ export function getDynamoDbClient(): DynamoDB {
     const ddb = new DynamoDB({
         apiVersion: '2012-08-10',
         region: 'us-west-2',
-        credentials: {
-            accessKeyId: process.env['CL_AWS_ACCESS_KEY'] as string,
-            secretAccessKey: process.env['CL_AWS_SECRET_ACCESS_KEY'] as string,
-        },
+        // credentials: {
+        //     accessKeyId: process.env['CL_AWS_ACCESS_KEY'] as string,
+        //     secretAccessKey: process.env['CL_AWS_SECRET_ACCESS_KEY'] as string,
+        // },
     });
 
     return ddb;
