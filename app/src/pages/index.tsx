113d716123f34ed82311c8c9ab24c4b30c95830f
Andrey Butenko <andreyxbutenko@gmail.com>
2023-06-12 22:14:38 -0700

    Use link isntead of a

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
     return (
@@ -8,7 +9,8 @@ export default function Home() {
                 <title>CL311</title>
             </Head>
             <main className={`${styles.main}`}>
-                Hello! <a href="/guestbook">Guestbook</a>
+                Hello!
+                <Link href="/guestbook">Guestbook</Link>
             </main>
         </>
     );
