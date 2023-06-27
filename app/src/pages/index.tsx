d7facfe44c9c09398e656ceebfadb58d4b6c62b6
Andrey Butenko <andreyxbutenko@gmail.com>
2023-06-26 22:37:06 -0700

    Add home page

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,6 +1,8 @@
+import PageHeader from '@/components/PageHeader';
 import Head from 'next/head';
-import styles from '@/styles/Home.module.css';
 import Link from 'next/link';
+import Container from 'react-bootstrap/Container';
+import Stack from 'react-bootstrap/Stack';
 
 export default function Home() {
     return (
@@ -8,9 +10,17 @@ export default function Home() {
             <Head>
                 <title>CL311</title>
             </Head>
-            <main className={`${styles.main}`}>
-                Hello!
-                <Link href="/guestbook">Guestbook</Link>
+            <main>
+                <Container>
+                    <Stack gap={3}>
+                        <PageHeader>CL311 Home Page</PageHeader>
+                        <p>Welcome to CodeDay Labs 311!</p>
+                        <p>
+                            While we get set up, take a look at our{' '}
+                            <Link href="/guestbook">guestbook</Link>.
+                        </p>
+                    </Stack>
+                </Container>
             </main>
         </>
     );
