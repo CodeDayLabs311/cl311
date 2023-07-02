56bd9ccc49e1cffa0c4cf4e88691477e0cf4fdeb
Andrey Butenko <andreyxbutenko@gmail.com>
2023-07-01 20:13:23 -0700

    Use us-east-1

--- app/src/utils/api/dynamodb.ts
+++ app/src/utils/api/dynamodb.ts
@@ -4,7 +4,7 @@ import { DynamoDB } from '@aws-sdk/client-dynamodb';
 export function getDynamoDbClient(): DynamoDB {
     const ddb = new DynamoDB({
         apiVersion: '2012-08-10',
-        region: 'us-west-2',
+        region: 'us-east-1',
         // credentials: {
         //     accessKeyId: process.env['CL_AWS_ACCESS_KEY'] as string,
         //     secretAccessKey: process.env['CL_AWS_SECRET_ACCESS_KEY'] as string,
--- infra-cdk/lib/core/constants/environment.ts
+++ infra-cdk/lib/core/constants/environment.ts
@@ -1,1 +1,1 @@
-export const ENVIRONMENT = { account: '377019892688', region: 'us-west-2' };
+export const ENVIRONMENT = { account: '377019892688', region: 'us-east-1' };
