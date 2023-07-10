70dda3274a93af04709ce5e713b079e3b7778cfb
Zhanping Zeng <66219129+ZhanpZ@users.noreply.github.com>
2023-07-10 12:16:19 -0400

    Add MUI
    MUI

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function Home() {
     return (
@@ -15,6 +16,7 @@ export default function Home() {
                     <Stack gap={3}>
                         <PageHeader>CL311 Home Page</PageHeader>
                         <p>Welcome to CodeDay Labs 311!</p>
+                        <DeleteIcon/><DeleteIcon color="primary" /><DeleteIcon color="secondary" /><DeleteIcon color="success" /><DeleteIcon color="action" /><DeleteIcon color="disabled" />
                         <p>
                             While we get set up, take a look at our{' '}
                             <Link href="/guestbook">guestbook</Link>.
