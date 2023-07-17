d03ea2e367a20b5721e7273d009d6663b024c75a
MinieMoe <minnguyen7.2001@gmail.com>
2023-07-17 15:49:35 -0700

    Add listReport API

--- app/src/hooks/report/useReportClient.ts
+++ app/src/hooks/report/useReportClient.ts
@@ -1,1 +1,9 @@
-import { IReportClient } from "@/models";
+import { IReportClient } from '@/models';
+import { ReportApiClient } from '@/utils';
+import { useMemo } from 'react';
+
+export const useReportClient = (): IReportClient => {
+    const reportClient = useMemo<IReportClient>(() => new ReportApiClient(), []);
+   
+    return reportClient;
+};
---
+++ app/src/pages/api/report/list/index.ts
@@ -0,0 +1,49 @@
+import {
+    HttpMethod,
+    IApiErrorResponse,
+    IReport,
+    INTERNAL_SERVER_ERROR,
+    METHOD_NOT_ALLOWED,
+} from '@/models';
+import { GuestBookDbClient } from '@/utils';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export interface IListReportResponse {
+    /** Guest book messages */
+    messages: IReport[];
+    /** Pagination token */
+    paginationToken: string | undefined;
+}
+
+// TODO: need to import ReportDbClient from Sophie
+/**
+ * List guest book messages
+ *
+ * Allowed methods: GET
+ *
+ * Response: IListReportResponse
+ *
+ * Potential errors:
+ *  - 405: when non-allowed method is used
+ *  - 500: internal server error
+ */
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<IListReportResponse | IApiErrorResponse>
+) {
+    if (req.method !== HttpMethod.GET) {
+        return res.status(405).send({ message: METHOD_NOT_ALLOWED });
+    }
+
+    try {
+        const guestBookClient = new GuestBookDbClient();
+
+        const { messages, paginationToken } = await guestBookClient.listMessages();
+
+        return res.status(200).json({ messages, paginationToken });
+    } catch (err) {
+        console.error(err);
+
+        return res.status(500).send({ message: INTERNAL_SERVER_ERROR });
+    }
+}
