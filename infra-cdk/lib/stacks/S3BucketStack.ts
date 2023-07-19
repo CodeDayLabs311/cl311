1476e2a4915728dd169b9481f8fac3777f202352
ZhanpZ <66219129+ZhanpZ@users.noreply.github.com>
2023-07-19 19:18:16 -0400

    Updating S3BucketStack.ts

---
+++ infra-cdk/lib/stacks/S3BucketStack.ts
@@ -0,0 +1,30 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+import { Stage, Tenant } from '../core/enums';
+import { BaseStackProps } from '../core/types';
+
+const BASE_TABLE_NAME = 'S3Bucket';
+
+export class S3BucketStack extends cdk.Stack{
+    public readonly bucket: s3.Bucket;
+    private stage: Stage;
+    private tenant: Tenant;
+
+    constructor(scope: Construct, id: string, props: BaseStackProps) {
+        super(scope, id, props);
+
+        this.stage = props.stage;
+        this.tenant = props.tenant;
+
+        this.bucket = new s3.Bucket(this, "s3-image-demo",{
+            versioned: false,
+            bucketName: this.getBucketName(),
+            publicReadAccess: false,
+            removalPolicy: cdk.RemovalPolicy.RETAIN
+        });
+    }
+    getBucketName(): string {
+        return `${BASE_TABLE_NAME}-${this.stage}-${this.tenant}`;
+    }
+}
