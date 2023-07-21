1085a71872aff50bcc6be03642524ec51d021023
MinieMoe <minnguyen7.2001@gmail.com>
2023-07-21 14:07:51 -0700

    Fix conflicts

--- app/src/components/report/Table.tsx
+++ app/src/components/report/Table.tsx
@@ -36,54 +36,6 @@ const columnWidth = {
     actions: 170,
 };
 
-const rows: GridRowsProp = [
-    {
-        reportId: 'report1',
-        name: 'John Doe',
-        emailAddress: 'john.doe@example.com',
-        phoneNumber: '123-456-7890',
-        reportCategory: ['Illegal Dumping'],
-        address: '123 Main St',
-        gpsCoordinates: '12.34,56.78',
-        issueDescription: 'Pls send help, it stinks',
-        attachments: 'link to attachments',
-        email: true,
-        sms: false,
-        statusOfReport: 'Reported',
-        dateTimeOfSubmission: '07/01/2023',
-    },
-    {
-        reportId: 'report2',
-        name: 'Jane Doe',
-        emailAddress: 'jane.doe@example.com',
-        phoneNumber: '206-xxx-xxx',
-        reportCategory: ['Clogged Drain'],
-        address: '456 Main St',
-        gpsCoordinates: '21.34,56.60',
-        issueDescription: 'Help pls, the street is flooded...',
-        attachments: 'image',
-        email: true,
-        sms: false,
-        statusOfReport: 'Done',
-        dateTimeOfSubmission: '06/20/2023',
-    },
-    {
-        reportId: 'report3',
-        name: '',
-        emailAddress: '',
-        phoneNumber: '',
-        reportCategory: ['Illegal Dumping', 'Clogged Drain', 'Arson', 'Auto Theft', 'Other'],
-        address: '789 Main St',
-        gpsCoordinates: '21.34,56.60',
-        issueDescription: 'I trashed and flooded the street >:)',
-        attachments: 'image',
-        email: true,
-        sms: false,
-        statusOfReport: 'In Progress',
-        dateTimeOfSubmission: '06/30/2023',
-    },
-];
-
 function getChipProps(params: GridRenderCellParams): ChipProps {
     const status = params.value;
     switch (status) {
@@ -259,7 +211,7 @@ export default function Table({ rows }: TableProps) {
     ];
 
     return (
-        <div style={{ height: '100%', width: '100%', minWidth: '800px'}}>
+        <div style={{ height: '100%', width: '100%', minWidth: '800px' }}>
             <DataGrid
                 getRowHeight={() => 'auto'}
                 rows={rows}
