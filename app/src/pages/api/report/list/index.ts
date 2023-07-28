64a275dfc67d0578b33245e7646a11116bdb22ef
MinieMoe <minnguyen7.2001@gmail.com>
2023-07-28 14:58:24 -0700

    Dump all files from min-sortAndFilter

--- app/src/pages/api/report/list/index.ts
+++ app/src/pages/api/report/list/index.ts
@@ -4,11 +4,12 @@ import {
     IReport,
     INTERNAL_SERVER_ERROR,
     METHOD_NOT_ALLOWED,
+    BAD_REQUEST,
 } from '@/models';
+import { isUndefined } from '@/utils';
 import { ReportDbClient } from '@/utils/clients/ReportDbClient';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-/** Return a list of report and pagination token upon a LIST request */
 export interface IListReportResponse {
     /** Reports */
     reports: IReport[];
@@ -16,65 +17,8 @@ export interface IListReportResponse {
     paginationToken: string | undefined;
 }
 
-/** NOTE: function to mock DB lisitng because I don't have access to create report page or permission to create new item in AWS
-    TODO: will need to switch to real DB function eventually
-*/
-const mockListReportsFromDb = (): { reports: IReport[]; paginationToken: string | undefined } => {
-    return {
-        reports: [
-            {
-                reportId: 'report1',
-                name: 'John Doe',
-                emailAddress: 'john.doe@example.com',
-                phoneNumber: '123-456-7890',
-                reportCategory: 'Illegal Dumping',
-                address: '123 Main St',
-                gpsCoordinates: '12.34,56.78',
-                issueDescription: 'Pls send help, it stinks',
-                attachments: 'link to attachments',
-                email: true,
-                sms: false,
-                statusOfReport: 'Submitted',
-                dateTimeOfSubmission: '07/01/2023',
-            },
-
-            {
-                reportId: 'report2',
-                name: 'Jane Doe',
-                emailAddress: 'jane.doe@example.com',
-                phoneNumber: '206-xxx-xxx',
-                reportCategory: 'Clogged Drain',
-                address: '456 Main St',
-                gpsCoordinates: '21.34,56.60',
-                issueDescription: 'Help pls, the street is flooded...',
-                attachments: 'image',
-                email: true,
-                sms: false,
-                statusOfReport: 'In Progress',
-                dateTimeOfSubmission: '06/20/2023',
-            },
-            {
-                reportId: 'report3',
-                name: '',
-                emailAddress: '',
-                phoneNumber: '',
-                reportCategory: 'Other',
-                address: '789 Main St',
-                gpsCoordinates: '21.34,56.60',
-                issueDescription: 'I trashed and flooded the street >:)',
-                attachments: 'image',
-                email: true,
-                sms: false,
-                statusOfReport: 'Done',
-                dateTimeOfSubmission: '06/30/2023',
-            },
-        ],
-        paginationToken: undefined,
-    };
-};
-
 /**
- * List Reports
+ * Return a list of report and pagination token upon a LIST request
  *
  * Allowed methods: GET
  *
@@ -92,14 +36,26 @@ export default async function handler(
         return res.status(405).send({ message: METHOD_NOT_ALLOWED });
     }
 
+    const status = typeof req.query.status === 'string' ? req.query.status : undefined;
+    const ascending =
+        typeof req.query.ascending === 'string' ? req.query.ascending === 'true' : undefined;
+
     try {
-        /** Uncomment these lines to switch to the real DB */
         const reportClient = new ReportDbClient();
 
-        const { reports, paginationToken } = await reportClient.listReports();
-
-        // const { reports, paginationToken } = mockListReportsFromDb();
-        return res.status(200).json({ reports, paginationToken });
+        if (status) {
+            const { reports, paginationToken } = await reportClient.listReportsByStatus(status);
+            return res.status(200).json({ reports, paginationToken });
+        } else if (status && ascending) {
+            const { reports, paginationToken } = await reportClient.listReportsByStatus(
+                status,
+                ascending
+            );
+            return res.status(200).json({ reports, paginationToken });
+        } else {
+            const { reports, paginationToken } = await reportClient.listReports();
+            return res.status(200).json({ reports, paginationToken });
+        }
     } catch (err) {
         console.error(err);
 
