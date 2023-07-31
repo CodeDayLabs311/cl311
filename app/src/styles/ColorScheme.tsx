2591210779cf96c59b2912d516a5fd3acc6aa920
Sophie Nguyen <sophiengocnguyen113@gmail.com>
2023-07-31 14:10:53 -0400

    Follow Up Viewing Page

---
+++ app/src/styles/ColorScheme.tsx
@@ -0,0 +1,6 @@
+/** Color Scheme */
+export const LIGHT_BLUE = '#ECF2F8';
+export const MILD_BLUE = '#96b6d4';
+export const DARK_BLUE = '#477096';
+export const BLACK = '#4F4846';
+export const WHITE = '#FDF8F5';
