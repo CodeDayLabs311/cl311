d5ff6ebc2fc76f64cfd7b6d6c151436154ca110d
Sophie Nguyen <sophiengocnguyen113@gmail.com>
2023-08-02 14:18:18 -0400

    Changed Back to Original Codebase

--- app/src/pages/api/report/create.ts
+++ app/src/pages/api/report/create.ts
@@ -5,7 +5,6 @@ import {
     IReport,
     INTERNAL_SERVER_ERROR,
     METHOD_NOT_ALLOWED,
-    MISSING_REQUEST_BODY,
 } from '@/models';
 import { ReportDbClient, isUndefined } from '@/utils';
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -25,50 +24,41 @@ export interface ICreateReportResponse {
  * Response: IGetReportResponse
  *
  * Potential errors:
- *  - 200: when report is successfully created
  *  - 400: when body is invalid or not provided
  *  - 405: when non-allowed method is used
  *  - 500: internal server error
  */
-
-async function validateRequestBody(body: any): Promise<IReport> {
-    if (!body || typeof body !== 'object') {
-        throw new Error(MISSING_REQUEST_BODY);
-    }
-
-    return body as IReport;
-}
-
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ICreateReportResponse | IApiErrorResponse>
 ) {
+    if (req.method !== HttpMethod.POST) {
+        return res.status(405).send({ message: METHOD_NOT_ALLOWED });
+    }
+
     try {
-        switch (req.method) {
-            case HttpMethod.POST:
-                const params = await validateRequestBody(req.body);
-                const reportClient = new ReportDbClient();
-                const isValidRequest = isUndefined(params.reportId);
+        const reportClient = new ReportDbClient();
 
-                if (!isValidRequest) {
-                    // Don't store bad data in the database!
-                    return res.status(400).json({ message: BAD_REQUEST });
-                }
+        const params = req.body as IReport;
+        const isValidRequest =
+            isUndefined(params?.reportId); 
 
-                const report = await reportClient.createReport(params);
-
-                if (isUndefined(report)) {
-                    // Report was successfully created but could not be found
-                    return res.status(500).json({ message: INTERNAL_SERVER_ERROR });
-                }
-
-                return res.status(200).json({ report: report! });
-
-            default:
-                return res.status(405).json({ message: METHOD_NOT_ALLOWED });
+        if (!isValidRequest) {
+            // Don't store bad data in the database!
+            return res.status(400).send({ message: BAD_REQUEST });
         }
+        
+        const report = await reportClient.createReport(params);
+
+        if (isUndefined(report)) {
+            // Report was successfully created but could not be found
+            return res.status(500).send({ message: INTERNAL_SERVER_ERROR });
+        }
+
+        return res.status(200).json({ report: report! });
     } catch (err) {
         console.error(err);
-        return res.status(500).json({ message: INTERNAL_SERVER_ERROR });
+
+        return res.status(500).send({ message: INTERNAL_SERVER_ERROR });
     }
 }
