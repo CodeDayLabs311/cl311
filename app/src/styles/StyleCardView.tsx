3fe897c0444b29f9bae1e8ed93d0f65a5c2bf9ec
Sophie Nguyen <sophiengocnguyen113@gmail.com>
2023-08-02 11:49:34 -0400

    Addressed Comments

--- app/src/styles/StyleCardView.tsx
+++ app/src/styles/StyleCardView.tsx
@@ -1,21 +1,27 @@
 import { styled } from '@mui/material/styles';
 import { Box, Typography } from '@mui/material';
 import ButtonLink from '@/components/ButtonLink';
-import {LIGHT_BLUE, MILD_BLUE, DARK_BLUE } from '@/styles/ColorScheme';
+import {
+    LIGHT_PRIMARY,
+    MEDIUM_PRIMARY,
+    DARK_PRIMARY,
+    PRIMARY_TEXT,
+    PRIMARY_BACKGROUND,
+} from '@/styles/ColorScheme';
 
 // Define custom styles
 const StyledReportDetails = styled(Box)({
     '& p': {
         margin: 0,
         padding: '5px 0',
-        color: `black`,
+        color: PRIMARY_TEXT,
     },
     '& strong': {
-        color: `black`,
+        color: PRIMARY_TEXT,
         textAlign: 'center',
     },
     borderRadius: '2px',
-    background: `white`,
+    background: PRIMARY_BACKGROUND,
     boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.3)',
 });
 
@@ -27,50 +33,50 @@ const GroupedBox = styled(Box)({
     marginRight: '15px',
     border: '2px solid #ccc',
     borderRadius: '30px',
-    background: `linear-gradient(45deg, ${LIGHT_BLUE}, white)`,
+    background: `linear-gradient(45deg, ${LIGHT_PRIMARY}, ${PRIMARY_BACKGROUND})`,
     boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.3)',
 
     '&:hover': {
-        background: `linear-gradient(45deg, white, ${LIGHT_BLUE})`,
+        background: `linear-gradient(45deg, ${PRIMARY_BACKGROUND}, ${LIGHT_PRIMARY})`,
     },
 });
 
 const Heading = styled('span')({
-    color: `${DARK_BLUE}`,
+    color: DARK_PRIMARY,
     fontWeight: 'bold',
     marginBottom: '10px',
     fontSize: '1.3rem',
     fontFamily: "'Roboto', sans-serif",
-    lineHeight: 1.5, 
+    lineHeight: 1.5,
 });
 
 const FooterDetails = styled(Box)({
     borderRadius: '2px',
-    background: `white`,
+    background: PRIMARY_BACKGROUND,
     boxShadow:
         '-5px 0 5px -5px rgba(0, 0, 0, 0.3), 5px 0 5px -5px rgba(0, 0, 0, 0.3), 0 5px 5px -5px rgba(0, 0, 0, 0.3)',
 });
 
 const IDText = styled('span')({
-    color: `${DARK_BLUE}`,
+    color: DARK_PRIMARY,
     fontWeight: 'bold',
 });
 
 const PageTitle = styled(Typography)({
-    color: `${DARK_BLUE}`,
+    color: DARK_PRIMARY,
     fontWeight: 'bold',
     textAlign: 'center',
 });
 
 const StyledButton = styled(ButtonLink)({
     borderRadius: '10px',
-    background: `linear-gradient(45deg, ${MILD_BLUE}, white)`,
-    color: `black`,
+    background: `linear-gradient(45deg, ${MEDIUM_PRIMARY}, ${PRIMARY_BACKGROUND})`,
+    color: PRIMARY_TEXT,
     border: '2px solid #ccc',
     boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.3)',
     fontWeight: 'bold',
     '&:hover': {
-        background: `linear-gradient(45deg, white, ${MILD_BLUE})`,
+        background: `linear-gradient(45deg, ${PRIMARY_BACKGROUND}, ${MEDIUM_PRIMARY})`,
         color: 'blue',
         border: '2px solid blue',
     },
