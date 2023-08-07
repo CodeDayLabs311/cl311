b2f1a5ac47d6e14c1baf4a2eb08007ddeccfbec7
Sophie Nguyen <sophiengocnguyen113@gmail.com>
2023-08-07 12:00:30 -0400

    Fixed Deploy

--- app/src/pages/api/report/list/index.ts
+++ app/src/pages/api/report/list/index.ts
@@ -16,63 +16,6 @@ export interface IListReportResponse {
     paginationToken: string | undefined;
 }
 
-/** NOTE: function to mock DB lisitng because I don't have access to create report page or permission to create new item in AWS
-    TODO: will need to switch to real DB function eventually
-*/
-const mockListReportsFromDb = (): { reports: IReport[]; paginationToken: string | undefined } => {
-    return {
-        reports: [
-            {
-                reportId: 'report1',
-                name: 'John Doe',
-                emailAddress: 'john.doe@example.com',
-                phoneNumber: '123-456-7890',
-                reportCategory: 'Illegal Dumping',
-                address: '123 Main St',
-                gpsCoordinates: '12.34,56.78',
-                issueDescription: 'Pls send help, it stinks',
-                attachments: 'link to attachments',
-                email: true,
-                sms: false,
-                statusOfReport: 'Submitted',
-                dateTimeOfSubmission: '07/01/2023',
-            },
-
-            {
-                reportId: 'report2',
-                name: 'Jane Doe',
-                emailAddress: 'jane.doe@example.com',
-                phoneNumber: '206-xxx-xxx',
-                reportCategory: 'Clogged Drain',
-                address: '456 Main St',
-                gpsCoordinates: '21.34,56.60',
-                issueDescription: 'Help pls, the street is flooded...',
-                attachments: 'image',
-                email: true,
-                sms: false,
-                statusOfReport: 'In Progress',
-                dateTimeOfSubmission: '06/20/2023',
-            },
-            {
-                reportId: 'report3',
-                name: '',
-                emailAddress: '',
-                phoneNumber: '',
-                reportCategory: 'Other',
-                address: '789 Main St',
-                gpsCoordinates: '21.34,56.60',
-                issueDescription: 'I trashed and flooded the street >:)',
-                attachments: 'image',
-                email: true,
-                sms: false,
-                statusOfReport: 'Done',
-                dateTimeOfSubmission: '06/30/2023',
-            },
-        ],
-        paginationToken: undefined,
-    };
-};
-
 /**
  * List Reports
  *
@@ -84,6 +27,7 @@ const mockListReportsFromDb = (): { reports: IReport[]; paginationToken: string 
  *  - 405: when non-allowed method is used
  *  - 500: internal server error
  */
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<IListReportResponse | IApiErrorResponse>
@@ -93,12 +37,10 @@ export default async function handler(
     }
 
     try {
-        /** Uncomment these lines to switch to the real DB */
         const reportClient = new ReportDbClient();
 
         const { reports, paginationToken } = await reportClient.listReports();
 
-        // const { reports, paginationToken } = mockListReportsFromDb();
         return res.status(200).json({ reports, paginationToken });
     } catch (err) {
         console.error(err);
