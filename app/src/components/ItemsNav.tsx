0fd3ce0f6303f6a78cf2078365d0421979450261
Sophie Nguyen <sophiengocnguyen113@gmail.com>
2023-08-07 17:59:58 -0400

    Fixed pathname

--- app/src/components/ItemsNav.tsx
+++ app/src/components/ItemsNav.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
 import { ItemsHeader, BoldText } from '@/styles/StyleHomePage';
 
 export default function ItemsNav() {
-
-    const pathname = usePathname()
+    const pathname = usePathname();
     const [value, setValue] = React.useState(pathname);
 
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
@@ -18,26 +17,22 @@ export default function ItemsNav() {
             <Tabs
                 value={value}
                 onChange={handleChange}
-                textColor="primary"
-                indicatorColor="primary"
+                textColor="primary" 
+                indicatorColor="primary" 
                 aria-label="primary tabs example"
             >
                 <Tab value="/dashboard" label={<BoldText>Dashboard</BoldText>} href="/dashboard" />
-
                 <Tab
                     value="/report/create"
                     label={<BoldText>Add Report</BoldText>}
                     href="/report/create"
                 />
-
                 <Tab value="/report" label={<BoldText>Report List</BoldText>} href="/report" />
-
                 <Tab
                     value="/guestbook/create"
                     label={<BoldText>Add Guestbook</BoldText>}
                     href="/guestbook/create"
                 />
-
                 <Tab
                     value="/guestbook"
                     label={<BoldText>Guestbook List</BoldText>}
