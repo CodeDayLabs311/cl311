99bacf4b1c7c202bea80aa69f033f0f716a3af6f
MinieMoe <minnguyen7.2001@gmail.com>
2023-08-07 16:59:00 -0700

    Setting up marker with coordinate printed out to a text field

--- app/src/components/report/LocationPicker.tsx
+++ app/src/components/report/LocationPicker.tsx
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import Map, { Marker } from 'react-map-gl';
+import Map, { Marker, LngLat, MarkerDragEvent } from 'react-map-gl';
+import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { Form } from 'react-bootstrap';
 
 export default function LocationPicker() {
-
     const [viewState, setViewState] = useState({
         longitude: -122.335167,
         latitude: 47.608013,
         zoom: 10.5,
     });
 
-    // const [reportLocation, setReportLocation] = useState<MapDragEvent | {} >({})
+    const [reportLocation, setReportLocation] = useState<number[]>([]);
+
+    function handleDragEnd(event: MarkerDragEvent) {
+        const { lng, lat } = event.lngLat;
+        setReportLocation([lng, lat]);
+        setViewState((prev) => ({
+            ...prev,
+            longitude: lng,
+            latitude: lat,
+        }));
+    }
 
     return (
         <div
@@ -28,19 +38,17 @@ export default function LocationPicker() {
                 mapStyle="mapbox://styles/mapbox/streets-v9"
             >
                 <Marker
-                    longitude={-122.335167}
-                    latitude={47.608013}
+                    longitude={viewState.longitude}
+                    latitude={viewState.latitude}
                     color="red"
                     draggable={true}
-                    onDragEnd={(event) => {
-                        console.log(event.lngLat);
-                    }}
+                    onDragEnd={handleDragEnd}
                 />
             </Map>
 
             <Form className="locationSearchBar">
                 <Form.Group>
-                    <Form.Control placeholder="Coordinate" />
+                    <Form.Control placeholder="Coordinate" value={reportLocation.map(String)} />
                 </Form.Group>
             </Form>
         </div>
